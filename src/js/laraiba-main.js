66cac66f2872d0eb884dbc5b3f4515121ee2791b
Andy Librian <andyajadeh@gmail.com>
2014-02-07 02:08:03 +0000

    temporarily remove search btn

--- src/js/laraiba-main.js
+++ src/js/laraiba-main.js
@@ -2,10 +2,9 @@ $(function() {
     var Lrq = LaRaiba.Quran;
         
     var $topNavbar = $('<div id="top-navbar" class="navbar navbar-inverse navbar-top"></div>');
-    $('<a href="#" class="dropdown-toggle" data-toggle="dropdown" id="main-menu-btn" title="Menu"><span class="glyphicon glyphicon-list"></span></a>' +
-    '<ul class="dropdown-menu" role="menu"><li><a href="#">Tentang LaRaiba</a></li><li><a href="#">Development</a></li></ul>' +
+    $('<a href="#" id="main-menu-btn" title="Menu"><span class="glyphicon glyphicon-list"></span></a>' +
     '<a class="sura-navigator" href="#"></a>' +
-    '<a href="#" title="Pencarian" id="btn-main-search" class="pull-right"><span class="glyphicon glyphicon-search"></span></a>').appendTo($topNavbar);
+    '').appendTo($topNavbar);
     $topNavbar.appendTo('#wrap');
 
     var $quranContent = $('<div id="quran-content" class="content"></div>');
