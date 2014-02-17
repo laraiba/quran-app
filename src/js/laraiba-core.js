9354bdcd491bb93ecbbb30693febb6e78da18633
Andy Librian <andyajadeh@gmail.com>
2014-02-17 05:56:03 +0000

    improve code laraiba core

--- src/js/laraiba-core.js
+++ src/js/laraiba-core.js
@@ -1,22 +1,21 @@
-// init namespace
+// init namespaces
+
 var LaRaiba = LaRaiba || {};
 LaRaiba.Quran = LaRaiba.Quran || {};
 
-(function(Lr) {
-    var Lrq = Lr.Quran;
+(function(Lrq) {
+    Lrq.Text = {};
+    Lrq.Translations = {};
     
-    Lrq.Text = Lrq.Text || {};
-    Lrq.Translations = Lrq.Translations || {};
-    Lrq.Util = Lrq.Util || {};
-    
-    Lrq.Util.parseAyaId = function(ayaId) {
-        var split = ayaId.split(':');
-        
-        return {
-            suraIndex: split[0],
-            ayaIndex: split[1]
-        };
-    };
-    
+    Lrq.Util = {
+        parseAyaId: function(ayaId) {
+            var split = ayaId.split(':');
 
-})(LaRaiba);
+            return {
+                suraIndex: split[0],
+                ayaIndex: split[1]
+            };
+        }
+    };    
+
+})(LaRaiba.Quran);
